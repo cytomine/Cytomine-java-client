1f0f8e69521eb6c53e7b959c0a767c9df25f8145
GiMichiels <gino_michiels@hotmail.com>
2018-04-05 08:41:29 +0200

    Replace SoftwareRepository by SoftwareUserRepository

---
+++ src/main/java/be/cytomine/client/collections/SoftwareUserRepositoryCollection.java
@@ -0,0 +1,45 @@
+package be.cytomine.client.collections;
+
+/*
+ * Copyright (c) 2009-2018. Authors: see NOTICE file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import be.cytomine.client.models.SoftwareUserRepository;
+import org.json.simple.JSONObject;
+
+public class SoftwareUserRepositoryCollection extends Collection {
+
+    public SoftwareUserRepositoryCollection(int offset, int max) {
+        super(offset, max);
+    }
+
+    @Override
+    public String toURL() {
+        return getJSONResourceURL();
+    }
+
+    @Override
+    public String getDomainName() {
+        return "software_user_repository";
+    }
+
+    public SoftwareUserRepository get(int i) {
+        SoftwareUserRepository softwareUserRepository = new SoftwareUserRepository();
+        Object item = list.get(i);
+        softwareUserRepository.setAttr((JSONObject) item);
+        return softwareUserRepository;
+    }
+
+}
