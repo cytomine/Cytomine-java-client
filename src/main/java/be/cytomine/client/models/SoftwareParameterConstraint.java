eda6fd0592432bde5e50d573b905ce6fba6c65ad
13eMualliug <g.vissers13@gmail.com>
2019-04-02 10:41:01 +0200

    Pull request changes requested: change for the softwareParameterConstraint

--- src/main/java/be/cytomine/client/models/SoftwareParameterConstraint.java
+++ src/main/java/be/cytomine/client/models/SoftwareParameterConstraint.java
@@ -5,15 +5,10 @@ import be.cytomine.client.CytomineException;
 public class SoftwareParameterConstraint extends Model<SoftwareParameterConstraint> {
 
     public SoftwareParameterConstraint(){}
-    public SoftwareParameterConstraint(String value){
-        this.set("value", value);
-    }
 
-    public SoftwareParameterConstraint addSoftwareParameterConstraint(Long parameterConstraintId, Long softwareParameterId, String value) throws CytomineException {
-		SoftwareParameterConstraint softwareParameterConstraint = new SoftwareParameterConstraint();
-		softwareParameterConstraint.set("parameterConstraint", parameterConstraintId);
-		softwareParameterConstraint.set("softwareParameter", softwareParameterId);
-		softwareParameterConstraint.set("value", value);
-		return softwareParameterConstraint.save();
+    public SoftwareParameterConstraint(Long parameterConstraintId, Long softwareParameterId, String value) throws CytomineException {
+		this.set("parameterConstraint", parameterConstraintId);
+		this.set("softwareParameter", softwareParameterId);
+		this.set("value", value);
 	}
 }
