9eb18ba7564f37bfe786b37ee870711ef0044d9a
13eMualliug <g.vissers13@gmail.com>
2019-04-02 10:38:52 +0200

    Pull request changes requested: change for the processingServer constructor

--- src/main/java/be/cytomine/client/models/ProcessingServer.java
+++ src/main/java/be/cytomine/client/models/ProcessingServer.java
@@ -27,13 +27,23 @@ import be.cytomine.client.collections.ProcessingServerCollection;
  */
 public class ProcessingServer extends Model<ProcessingServer> {
     public ProcessingServer(){}
-    public ProcessingServer(String url){
-        this.set("url", url);
+    public ProcessingServer(String name,String host,String username,Integer port,String type,String processingMethodName,AmqpQueue amqpQueue,String persistentDirectory, String workingDirectory,Integer index){
+
+        this.set("name", name);
+        this.set("host", host);
+        this.set("username", username);
+        this.set("port", port);
+        this.set("type", type);
+        this.set("processingMethodName", processingMethodName);
+        this.set("amqpQueue", amqpQueue);
+        this.set("persistentDirectory", persistentDirectory);
+        this.set("workingDirectory", workingDirectory);
+        this.set("index", index);
     }
 
     @Override
     public String getDomainName() {
         return "processing_server";
     }
-    
+
 }
