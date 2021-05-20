748691d85a458d5435ee3c879025cd569c734c0b
lrollus <loic.rollus@cytomine.com>
2021-05-20 13:07:48 +0200

    build improvment (wip)

---
+++ scripts/ciPublishJar.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o xtrace
+set -o errexit
+
+echo "************************************** BUILD JAR ******************************************"
+
+file='./ci/version'
+VERSION_NUMBER=$(<"$file")
+
+echo "Build jar for $VERSION_NUMBER"
+
+echo "GPG_KEYNAME = $GPG_KEYNAME"
+
+docker build --rm -f scripts/Dockerfile-jar.build -t cytomine/cytomine-java-client-jar-builder \
+        --build-arg VERSION_NUMBER=$VERSION_NUMBER  \
+        --build-arg GPG_KEYNAME=$GPG_KEYNAME  \
+        --build-arg GPG_PASSPHRASE=$GPG_PASSPHRASE  \
+        --build-arg GPG_KEY=$GPG_KEY  \
+        --build-arg OSSRH_USER=$OSSRH_USER  \
+        --build-arg OSSRH_PASSWORD=$OSSRH_PASSWORD .
+
+containerId=$(docker create cytomine/cytomine-java-client-jar-builder)
+
+docker rm $containerId
