6f4da3ca47a328e641a306d30e7207e0711a8203
lrollus <loic.rollus@cytomine.com>
2021-06-03 09:46:54 +0200

    support vx.y.z tag versionning instead of x.y.z

--- scripts/ciBuildVersion.sh
+++ scripts/ciBuildVersion.sh
@@ -12,9 +12,9 @@ branchName=${1:-$(git rev-parse --abbrev-ref HEAD)} #if no argument defined, get
 
 echo "BANCH NAME IS '$branchName'"
 #in the master branch, make sure the tag is clean ('1.2.3'; not 1.2.3-alpha) and there has been 0 commits since the tag has been set.
-if [[ $gitLongTag =~ [0-9]+.[0-9]+.[0-9]+-0-[0-9a-g]{8,9}$ ]]; then
+if [[ $gitLongTag =~ v[0-9]+.[0-9]+.[0-9]+-0-[0-9a-g]{8,9}$ ]]; then
 
-  versionNumber=$(echo $gitLongTag | sed -r "s/([0-9]+\.[0-9]+\.[0-9]+)-[0-9]+-.+/\1/")
+  versionNumber=$(echo $gitLongTag | sed -r "s/v([0-9]+\.[0-9]+\.[0-9]+)-[0-9]+-.+/\1/")
 
 else
   echo "WARNING: invalid tag for a real release $gitLongTag"
